e46d033327eab490a8ba56d90bff179e04ba050e
Yaw Asare <yasare16@gmail.com>
2022-08-29 03:51:14 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useEffect, useState } from 'react';
+import DataTable from './DataTable/DataTable';
+import './DataTable/DataTable.css';
 
+//App begins here
 function App() {
+  const url = 'https://idme-interview.herokuapp.com'
+  const [purchases, setPurchases] = useState()
+  const [loading, setLoading] = useState(true)
+
+  
+  useEffect(() => {
+    const fetchPurchases = async () => {
+      const payload = await fetch(url)
+        .then(response => response.json())
+        .catch(response => console.error(response.status, response.text))
+      setPurchases(payload)
+    }
+    fetchPurchases()
+    setLoading(false)
+  }, [])
+  
+  if (loading)
+    return (
+      <div style={
+        {
+          height: '100vh',
+          paddingBlockStart: '50%',
+          textAlign: 'center'
+        }}>
+        Loading...
+      </div>
+  )
+  
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <main className="App-main">
+        <div className="content container">
+          <h1>Purchases</h1>
+          <DataTable data={purchases} />
+        </div>
+      </main>
     </div>
-  );
+    
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import DataTable from './DataTable/DataTable';
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+const mockResponse = [{
+    id: 1,
+    location: 'https://image.com',
+    purchaseDate: '2020-12-29T00:00:00.0000Z',
+    category: 'Foo',
+    description: 'Here is a description',
+    price: 2345,
+    name: 'Sample item'
+}]
+
+global.fetch = jest.fn(() => {
+  Promise.resolve({
+    json: () => Promise.resolve(mockResponse)
+  })
+})
+
+describe('App', () => {
+    beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockResponse)
+    })
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+    test('loads purchase history', async () => {
+      
+      const {container} = render(<App />);
+      expect(container).not.toBeEmptyDOMElement();
+      expect(screen.getByText('Sample item')).toBeInTheDocument()
+      
+    });
 });
